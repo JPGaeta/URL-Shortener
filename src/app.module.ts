eb695c39ad8395841d0f6d9750a565f9986c530b
JPGaeta <jpgaeta.dev@gmail.com>
2024-09-07 19:55:39 -0300

    feat: include links module on app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { LinksModule } from './links/links.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, AuthModule],
+  imports: [ConfigModule.forRoot(), UserModule, AuthModule, LinksModule],
 })
 export class AppModule {}
