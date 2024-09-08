e4cec7b4481a1c7b736be91f19808697a9efc1d0
JPGaeta <jpgaeta.dev@gmail.com>
2024-09-08 01:10:54 -0300

    fix: updating semantic-release to not commit dist

--- release.config.js
+++ release.config.js
@@ -6,7 +6,6 @@ const config = {
     [
       '@semantic-release/git',
       {
-        assets: ['dist/*.js', 'dist/*.js.map'],
         message:
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
