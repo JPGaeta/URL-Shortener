3f6a2190c8efd59a9420dde0623bc8731b9b62a4
JPGaeta <jpgaeta.dev@gmail.com>
2024-09-08 12:11:25 -0300

    feat: custom validator to validate url "https://"

--- src/links/dto/create-link.dto.ts
+++ src/links/dto/create-link.dto.ts
@@ -1,8 +1,38 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsUrl } from 'class-validator';
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+@ValidatorConstraint({ async: true })
+class IsHttpsConstraint implements ValidatorConstraintInterface {
+  validate(url: string) {
+    return url.startsWith('https://');
+  }
+
+  defaultMessage() {
+    return 'url must start with https://';
+  }
+}
+
+export function IsHttps(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsHttpsConstraint,
+    });
+  };
+}
 
 export class CreateLinkDto {
   @IsNotEmpty()
+  @IsHttps()
   @IsUrl()
   @ApiProperty({ required: true, description: 'The URL to be shortened' })
   url: string;
