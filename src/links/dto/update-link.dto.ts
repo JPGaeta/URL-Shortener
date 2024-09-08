75b63b35c5da7fa03635f85be2e80432b1f3e67e
JPGaeta <jpgaeta.dev@gmail.com>
2024-09-08 00:04:35 -0300

    fix: passing param to swagger

--- src/links/dto/update-link.dto.ts
+++ src/links/dto/update-link.dto.ts
@@ -1,9 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateLinkDto } from './create-link.dto';
 import { IsUUID } from 'class-validator';
 
 export class UpdateLinkDto extends CreateLinkDto {}
 
 export class UpdateLinkParamsDto {
+  @ApiProperty({ required: true })
   @IsUUID('7')
   id: string;
 }
